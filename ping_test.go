ab70324a4dac92e5fb847053c5ab27cee4b2ec2a
irom77 <irek.romaniuk@gmail.com>
2016-12-07 08:08:22 -0500

    Test added

--- ping_test.go
+++ ping_test.go
@@ -4,9 +4,12 @@ import (
 	"testing"
 
 	. "github.com/smartystreets/goconvey/convey"
+	"syscall"
 )
 
 func TestPing(t *testing.T) {
+	// we try if we have setuid bit
+	syscall.Setuid(0)
 	Convey("Ping", t, func() {
 		//func Ping(address string, timeout int) bool
 		So(Ping("8.8.8.8",1000), ShouldBeTrue)
